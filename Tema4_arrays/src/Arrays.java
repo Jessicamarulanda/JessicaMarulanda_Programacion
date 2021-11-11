01c5b9affbdbdd4ec217564efde220b959033cc3
Jessicamarulanda <92918303+Jessicamarulanda@users.noreply.github.com>
2021-11-11 11:30:34 +0100

    for

---
+++ Tema4_arrays/src/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main (String [] args){
+        int [] numeroDos = {1,2,3,4,5,6,7,8,9,10};
+        int longitudArray = numeroDos.length;
+        System.out.println(longitudArray);
+        System.out.println(numeroDos[9]);
+        for (int i = 0; i < longitudArray; i++){
+            System.out.println(numeroDos[i]);
+
+        }
+
+
+    }
+}
