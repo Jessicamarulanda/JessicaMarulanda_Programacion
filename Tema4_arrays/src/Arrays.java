23a9b4abfce3f0c25ab82b3c8f35e29ac2e0330a
Jessicamarulanda <92918303+Jessicamarulanda@users.noreply.github.com>
2021-11-12 12:06:48 +0100

    Update Arrays.java

--- Tema4_arrays/src/Arrays.java
+++ Tema4_arrays/src/Arrays.java
@@ -1,35 +1,32 @@
 public class Arrays {
-    public static void main (String [] args){
-        int[] numero = new int  [15];
-        int [] numeroDos = {1,2,3,4,5,6,7,8,9,10};
+    public static void main(String[] args) {
+        int[] numero = new int[15];
+        int[] numeroDos = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int numeroPares = 0;
-        int numeroImpares = 0 ;
+        int numeroImpares = 0;
         int longitudArray = numeroDos.length;
-       Object[] cosas = {1,"Hola",numero};
+        Object[] cosas = {1, "Hola", numero};
         System.out.println(longitudArray);
         System.out.println(numeroDos[9]);
-        for (int i = 0; i < longitudArray; i++){
+        for (int i = 0; i < longitudArray; i++) {
             System.out.println(numeroDos[i]);
         }
-             numeroDos [7] = 88;
+        numeroDos[7] = 88;
         System.out.println("Modificación");
-        for (int i = 0; i < longitudArray; i++){
+        for (int i = 0; i < longitudArray; i++) {
             System.out.println(numeroDos[i]);
         }
 
-        for (int i = 0; i <  numero.length; i++ ){
-            numero [i] = (int)(Math.random() *50) + 1;
-            System.out.println("El número Aleatorio es : " + numero[i] );
-            if((numero[i]%2) == 0 )  {
-                numeroPares ++;
+        for (int i = 0; i < numero.length; i++) {
+            numero[i] = (int) (Math.random() * 50) + 1;
+            System.out.println("El número Aleatorio es : " + numero[i]);
+            if ((numero[i] % 2) == 0) {
+                numeroPares++;
             } else {
-                numeroImpares ++ ;
+                numeroImpares++;
             }
         }
         System.out.println("La cantidad de números Pares son : " + numeroPares);
         System.out.println("La cantidad de números Impares son : " + numeroImpares);
     }
-
-
-
 }
