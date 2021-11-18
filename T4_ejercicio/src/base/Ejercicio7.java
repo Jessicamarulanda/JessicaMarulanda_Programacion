00437e7d6f4ab7b60f28bf91cab00837ed957b38
Jessicamarulanda <92918303+Jessicamarulanda@users.noreply.github.com>
2021-11-18 07:56:41 +0100

    terminar

---
+++ T4_ejercicio/src/base/Ejercicio7.java
@@ -0,0 +1,19 @@
+package base;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main (String[] args ) {
+        Scanner entradaTeclado = new Scanner(System.in);
+        String[] respuesta = new String[10];
+        for (int i = 0; i < respuesta.length; i++) {
+            System.out.printf("Ingrese la palabra a guardar");
+        respuesta[i] = entradaTeclado.nextLine();
+    }
+
+
+    
+
+
+    }
+}
