cd8ca13771195d481554cd89cad1eb845c2ecf45
Jessicamarulanda <92918303+Jessicamarulanda@users.noreply.github.com>
2022-04-07 11:35:59 +0200

    c

--- Tema_8/fichero_txt/src/Entrada.java
+++ Tema_8/fichero_txt/src/Entrada.java
@@ -6,15 +6,26 @@ import java.io.IOException;
 public class Entrada {
     public static void main(String[] args) {
         File file = null;
+        File file2 = null;
         File directory = null;
         File directory2 = null;
         file = new File("C:/Users/Usuario DAM1/Desktop/ficheros/fichero.txt");
+        file2 = new File("C:/Users/Usuario DAM1/Desktop/ficheros/fichero1.txt");
         directory = new File ("C:/Users/Usuario DAM1/Desktop/ficheros");
         directory2 = new File ("C:/Users/Usuario DAM1/Desktop/ficheros_2");
 
         FicherosController ficherosController = new FicherosController();
-        //ficherosController.getFileInfo(file);
+        ficherosController.getFileInfo(file);
         ficherosController.getDirectoryInfo(directory);
+
+        /*FicherosController ficherosController1 = new FicherosController();
+        ficherosController1.lecturaFichero(file);*/
+
+        /*FicherosController ficherosController1 = new FicherosController();
+        ficherosController1.lecturaBuffer(file);*/
+
+        FicherosController ficherosController1 = new FicherosController();
+        ficherosController1.escrituraFichero(file2);
 
     }
 }
--- Tema_8/fichero_txt/src/controller/FicherosController.java
+++ Tema_8/fichero_txt/src/controller/FicherosController.java
@@ -1,7 +1,6 @@
 package controller;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FicherosController {
 
@@ -21,8 +20,7 @@ public class FicherosController {
         }
 
     }
-    public void getDirectoryInfo(File file)
-    {
+    public void getDirectoryInfo(File file) {
         if(file.isDirectory())
         {
             String[] nombreFichero = file.list();
@@ -38,4 +36,80 @@ public class FicherosController {
         }
 
     }
+    public void lecturaFichero(File file) {
+        FileReader fileReader = null;
+
+        try {
+            int lectura = 0;
+            fileReader = new FileReader(file);
+           /* while(lectura != -1)
+            {
+                lectura = fileReader.read();
+                System.out.println(lectura);
+            }*/
+
+            while((lectura=fileReader.read()) != -1)
+            {
+                System.out.println((char)lectura);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fileReader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void lecturaBuffer(File file){
+
+        BufferedReader bufferedReader = null;
+        String linea;
+        String lecturaCompleta = "";
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+
+            while((linea= bufferedReader.readLine()) != null)
+            {
+                System.out.println(linea);
+                lecturaCompleta +=linea;
+                lecturaCompleta += "\n";
+            }
+            System.out.println("La lectura completa es: ");
+            System.out.println(lecturaCompleta);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void escrituraFichero (File file)
+    {
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file, true);
+            char letra;
+            int cifrado;
+            String lineaEscribir = "Esto es un ejemplo de una linea a escribir dentro del fichero ";
+            String[] listaletra = lineaEscribir.split("");
+            for (String item : listaletra) {
+                letra = (item.charAt(0));
+                cifrado = (((byte) letra) * 3) / 2;
+                fileWriter.write(cifrado);
+                System.out.println(cifrado);
+            }
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+        }
+    }
 }
+
